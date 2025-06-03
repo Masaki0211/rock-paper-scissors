4f7d29c2b3e56cf626b7ed67db4e0b05bd65e215
Masaki0211 <2511jinhwi@gmail.com>
2025-06-03 20:17:17 +0900

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,14 +1,28 @@
+# Rock Paper Scissors Game
+# Author: Masaki0211
+# This is a simple command-line game where the user plays against the computer.
+
 import random
+
 print("===================")
 print("Rock Paper Scissors")
 print("===================")
 print()
 print()
+
+
+#Displays options
 print("1) ✊")
 print("2) ✋")
 print("3) ✌️")
+
+#Get user input
 x = int(input("Pick a number:"))
+
+#Computer picks random number among 1 and 3
 y = random.randint(1,3)
+
+#prints outcome
 if x == 1 and y == 1:
     print("You chose: ✊")
     print("CPU chose: ✊")
